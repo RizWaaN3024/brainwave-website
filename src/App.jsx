7b7ec5a0cbdddc5f2d88dd932f9cd33c60337c59
Rizwaan <rizwaanans786@gmail.com>
2024-05-07 17:23:51 +0530

    Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
