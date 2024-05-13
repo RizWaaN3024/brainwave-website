e7989a3c8fb50605ad708f3d972f377501ed8a30
Rizwaan <rizwaanans786@gmail.com>
2024-05-14 00:09:28 +0530

    Added Benefits Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
