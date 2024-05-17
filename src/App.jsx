189fcb60129216d842c37050b2b605dee337cc2f
Rizwaan <rizwaanans786@gmail.com>
2024-05-18 00:39:11 +0530

    Added first section of Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -16,6 +16,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
