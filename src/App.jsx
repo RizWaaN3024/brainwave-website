ff60f9c4e1c6404f95858387fb3da5e07a5b3803
Rizwaan <rizwaanans786@gmail.com>
2024-05-21 01:03:32 +0530

    Added Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -21,6 +22,7 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
